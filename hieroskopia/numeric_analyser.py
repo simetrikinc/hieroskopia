66058c1347c21139f475205ded8b491acda1adf8
Albeiro Cuadrado <machado.albeiro@gmail.com>
2020-10-01 15:46:59 -0500

    Data type added to properties infered in numeric_analyser, improve on tests and regexps

--- hieroskopia/numeric_analyser.py
+++ hieroskopia/numeric_analyser.py
@@ -6,7 +6,7 @@ from pandas import Series
 from hieroskopia.utils.evaluator import Evaluator
 
 
-class InferNumeric():
+class InferNumeric:
     """
     Receive a column and try to analyze  the three digit separator,
     the decimal separator and get the numeric format pattern
@@ -19,20 +19,20 @@ class InferNumeric():
     def infer(series: Series):
         # Identify stage
         numeric_dict = {
-            # -1234 or -1234.12
-            r'^(?=.)([+-]?([0-9]*)(\.([0-9]+))?)$': {
+            # ($)(+-) 1234 or 1234.12
+            r'^([+-]?([0-9]*)(\.([0-9]+))?)$': {
                 'three_digit_separator': '',
                 'decimal_separator': '.'
             },
-            # -12,345.1234
-            "^[-]?\\d{1,3}(,\\d{3})*(\\.\\d+)?$": {
+            # ($)(+-) 12,345 or 12,345.1234
+            "^[+-]?\\d{1,3}(,\\d{3})*(\\.\\d+)?$": {
                 'three_digit_separator': ',',
                 'decimal_separator': '.'
             }
         }
 
         simple_int_pattern = r'[-+]?\d+'
-        chars_not_allowed = r'[^\d,.\s\-\+]'
+        chars_not_allowed = r'[^\d,.\-\+]'
 
         # Trim whitespaces and currencies, add 0 if start with .
         series = series.astype(str).str.replace(r'[\$€£¥ ]', '', regex=True)
@@ -40,9 +40,17 @@ class InferNumeric():
             [re.sub(r'^(?=.)([+-]?)(\.)([0-9]*)$', '\\g<1>0\\g<2>\\g<3>', str(s)) for s in
              series.values])) if Evaluator(
             series).series_contains(r'^(?=.)([+-]?)(\.)([0-9]*)$') else series
+
+        # If the series contains any number and not have any invalid character
         if Evaluator(series).series_match(simple_int_pattern) and not Evaluator(series).series_match(chars_not_allowed):
-            format_result = [numeric_format for (re_exp, numeric_format) in
-                             numeric_dict.items() if
-                             Evaluator(series).series_match(re_exp)]
-            return format_result
+            # Validate each regular expression into the series
+            for (re_exp, numeric_format) in numeric_dict.items():
+                # If any regexp match, save this as its format
+                if Evaluator(series).series_match(re_exp):
+                    format_result = numeric_format
+                    # Further, Identify if this series contains only integers or float
+                    # Classify as float if the series have the decimal separator found in the regexp
+                    format_result.update(type='float') if Evaluator(series).series_contains('\\' + format_result.get(
+                        'decimal_separator')) else format_result.update(type='integer')
+                    return [format_result]
         return []
