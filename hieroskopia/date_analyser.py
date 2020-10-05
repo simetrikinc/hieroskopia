76c892a94df750f3fb39cc31dfe728d6869b9c27
Albeiro Cuadrado <machado.albeiro@gmail.com>
2020-10-05 18:11:41 -0500

    added Snowflake support

--- hieroskopia/date_analyser.py
+++ hieroskopia/date_analyser.py
@@ -12,53 +12,54 @@ class InferDatetime:
     """
 
     @staticmethod
-    def infer(series: Series):
+    def infer(series: Series, return_format='C89'):
         generic_date_pattern = "\\d{1,4}(-|\\/)\\d{1,2}(-|\\/)\\d{1,4}(?:(T| )\\d{2}:\\d{2}(?::\\d{2})?(?:\\.\\d{3,6})?(?:Z|(?: )?AM|(?: )?PM|(?: )?am|(?: )?pm)?)?"
         hour_pattern = "\\d{2}:\\d{2}:\\d{2}"
         dates_dict = {
             # Todo: Add string dates like ( 03 May 2020)
             # 1930-08-05
-            "^\\d{4}-\\d{1,2}-\\d{1,2}$": "%Y-%m-%d",
+            "^\\d{4}-\\d{1,2}-\\d{1,2}$": {"C89": "%Y-%m-%d", 'snowflake': 'yyyy-mm-dd'},
             # 08-05-30
-            "^\\d{1,2}-\\d{1,2}-\\d{2}$": "%m-%d-%y",
+            "^\\d{1,2}-\\d{1,2}-\\d{2}$": {"C89": "%m-%d-%y", 'snowflake': 'mm-dd-yy'},
             # 08-05-1930
-            "^\\d{1,2}-\\d{1,2}-\\d{4}$": "%m-%d-%Y",
+            "^\\d{1,2}-\\d{1,2}-\\d{4}$": {"C89": "%m-%d-%Y", 'snowflake': 'mm-dd-yyyy'},
             # 8-5-30
-            "^\\d{1}-\\d{1}-\\d{2}$": "%-m/%-d/%y",
+            "^\\d{1}-\\d{1}-\\d{2}$": {"C89": "%-m/%-d/%y", 'snowflake': 'mm-dd-yy'},
             # 1930/08/05
-            "^\\d{4}/\\d{1,2}/\\d{1,2}$": "%Y/%m/%d",
+            "^\\d{4}/\\d{1,2}/\\d{1,2}$": {"C89": "%Y/%m/%d", 'snowflake': 'yyyy/mm/dd'},
             # 08/05/30
-            "^\\d{1,2}/\\d{1,2}/\\d{2}$": "%m/%d/%y",
+            "^\\d{1,2}/\\d{1,2}/\\d{2}$": {"C89": "%m/%d/%y", 'snowflake': 'mm/dd/yy'},
             # 08/05/1930
-            "^\\d{1,2}/\\d{1,2}/\\d{4}$": "%m-%d-%Y",
+            "^\\d{1,2}/\\d{1,2}/\\d{4}$": {"C89": "%m-%d-%Y", 'snowflake': 'mm/dd/yyyy'},
             # 8/5/30
-            "^\\d{1}/\\d{1}/\\d{2}$": "%-m/%-d/%y",
+            "^\\d{1}/\\d{1}/\\d{2}$": {"C89": "%-m/%-d/%y", 'snowflake': 'mm/dd/yy'},
         }
         datetime_dict = {
             # 1930-08-05 12:00:05
-            "^\\d{4}-\\d{1,2}-\\d{1,2}$": "%Y-%m-%d %H:%M:%S ",
+            "^\\d{4}-\\d{1,2}-\\d{1,2}$": {"C89": "%Y-%m-%d %H:%M:%S ", 'snowflake': 'yyyy-MM-dd hh:mi:ss'},
             # 08-05-30 12:00:05
-            "^\\d{1,2}-\\d{1,2}-\\d{2}$": "%m-%d-%y %H:%M:%S",
+            "^\\d{1,2}-\\d{1,2}-\\d{2}$": {"C89": "%m-%d-%y %H:%M:%S", 'snowflake': 'mm-dd-yy hh:mi:ss'},
             # 08-05-1930 12:00:05
-            "^\\d{1,2}-\\d{1,2}-\\d{4}$": "%m-%d-%Y %H:%M:%S",
+            "^\\d{1,2}-\\d{1,2}-\\d{4}$": {"C89": "%m-%d-%Y %H:%M:%S", 'snowflake': 'mm-dd-yyyy hh:mi:ss'},
             # 8-5-30 12:00:05
-            "^\\d{1}-\\d{1}-\\d{2}$": "%-m/%-d/%y %H:%M:%S",
+            "^\\d{1}-\\d{1}-\\d{2}$": {"C89": "%-m/%-d/%y %H:%M:%S", 'snowflake': 'mm-dd-yy hh:mi:ss'},
             # 1930/08/05 12:00:05
-            "^\\d{4}/\\d{1,2}/\\d{1,2}$": "%Y/%m/%d %H:%M:%S",
+            "^\\d{4}/\\d{1,2}/\\d{1,2}$": {"C89": "%Y/%m/%d %H:%M:%S", 'snowflake': 'yyyy/MM/dd hh:mi:ss'},
             # 08/05/30 12:00:05
-            "^\\d{1,2}/\\d{1,2}/\\d{2}$": "%m/%d/%y %H:%M:%S",
+            "^\\d{1,2}/\\d{1,2}/\\d{2}$": {"C89": "%m/%d/%y %H:%M:%S", 'snowflake': 'MM/dd/yy hh:mi:ss'},
             # 08/05/1930 12:00:05
-            "^\\d{1,2}/\\d{1,2}/\\d{4}$": "%m-%d-%Y %H:%M:%S",
+            "^\\d{1,2}/\\d{1,2}/\\d{4}$": {"C89": "%m-%d-%Y %H:%M:%S", 'snowflake': 'MM/dd/yyyy hh:mi:ss'},
             # 8/5/30 12:00:05
-            "^\\d{1}/\\d{1}/\\d{2}$": "%-m/%-d/%y %H:%M:%S",
+            "^\\d{1}/\\d{1}/\\d{2}$": {"C89": "%-m/%-d/%y %H:%M:%S", 'snowflake': 'MM/dd/yy hh:mi:ss'},
             # 2019-11-27T12:00:05.000Z
-            "^\\d{4}-\\d{1,2}-\\d{1,2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}Z$": "%Y-%m-%dT%H:%M:%S.%fZ"
+            "^\\d{4}-\\d{1,2}-\\d{1,2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}Z$": {"C89": "%Y-%m-%dT%H:%M:%S.%fZ",
+                                                                        'snowflake': 'yyyy-MM-ddTHH:mm:ss.SSZ'}
         }
         # Have this column a generic date format ?
         if Evaluator(series).series_match(generic_date_pattern):
             # Have this column time ?
             formats_dict = datetime_dict if Evaluator(series).series_contains(hour_pattern) else dates_dict
-            format_result = {'formats': [date_format for (re_exp, date_format) in formats_dict.items() if
+            format_result = {'formats': [date_format[return_format] for (re_exp, date_format) in formats_dict.items() if
                                          Evaluator(series).series_contains(re_exp)], 'type': 'datetime'}
             return format_result
 
