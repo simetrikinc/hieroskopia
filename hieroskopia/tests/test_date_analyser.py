85c7e52997359eb19c12b9e9346e573f46500697
Albeiro Cuadrado <machado.albeiro@gmail.com>
2021-01-29 19:16:56 -0500

    added dynamic discrimination on date_analyser between date and datetime output

--- hieroskopia/tests/test_date_analyser.py
+++ hieroskopia/tests/test_date_analyser.py
@@ -1,65 +1,95 @@
-import numpy as np
-import pandas as pd
-import pytest
+from pandas import Series
 
-from hieroskopia import InferDatetime
+from hieroskopia.utils.evaluator import Evaluator
 
 
-class TestDateAnalyser:
+class InferDatetime:
+    """
+    Receive a column and try to get the date or datetime
+    format pattern using regexp.
+    Return a dict with key named 'format'
+    with the pandas patterns
+    """
+
     @staticmethod
-    def date_analyser_unittest(expected_list: list, data: dict, return_format='C89'):
-        # Create test data frame
-        df = pd.DataFrame(data)
-        # Result list with format dicts
-        result_list = []
-        # Loop every df columns
-        for col in df.columns:
-            # Infer data format
-            result_list.append(InferDatetime.infer(df[col], return_format))
-        # Print lists
-        print('\n', '  Result list:', result_list, '\n', 'Expected list:',
-              expected_list)
-        # Test lists
-        assert expected_list == result_list
+    def infer(series: Series, return_format='C89'):
+        generic_date_pattern = "\\d{1,4}(-|\\/)\\d{1,2}(-|\\/)\\d{1,4}(?:(T| )\\d{2}:\\d{2}(?::\\d{2})?(?:\\.\\d{3,6})?(?:Z|(?: )?AM|(?: )?PM|(?: )?am|(?: )?pm)?)?"
+        hour_pattern = "\\d{2}:\\d{2}:\\d{2}"
+        dates_dict = {
+            # Todo: Add string dates like ( 03 May 2020)
+            # 1930-08-05
+            "^\\d{4}-\\d{1,2}-\\d{1,2}$": {"C89": "%Y-%m-%d", 'snowflake': 'yyyy-mm-dd', 'java': 'yyyy-MM-dd'},
+            # 08-05-30
+            "^\\d{1,2}-\\d{1,2}-\\d{2}$": {"C89": "%d-%m-%y", 'snowflake': 'dd-mm-yy', 'java': 'dd-MM-yy'},
+            # 08-05-1930
+            "^\\d{1,2}-\\d{1,2}-\\d{4}$": {"C89": "%d-%m-%Y", 'snowflake': 'dd-mm-yyyy', 'java': 'dd-MM-yyyy'},
+            # 8-5-30
+            "^\\d{1}-\\d{1}-\\d{2}$": {"C89": "%-d/%-m/%y", 'snowflake': 'dd-mm-yy', 'java': 'd-M-yy'},
+            # 1930/08/05
+            "^\\d{4}/\\d{1,2}/\\d{1,2}$": {"C89": "%Y/%m/%d", 'snowflake': 'yyyy/mm/dd', 'java': 'yyyy/MM/dd'},
+            # 08/05/30
+            "^\\d{1,2}/\\d{1,2}/\\d{2}$": {"C89": "%d/%m/%y", 'snowflake': 'dd/mm/yy', 'java': 'dd/MM/yy'},
+            # 08/05/1930
+            "^\\d{1,2}/\\d{1,2}/\\d{4}$": {"C89": "%d-%m-%Y", 'snowflake': 'dd/mm/yyyy', 'java': 'dd/MM/yyyy'},
+            # 8/5/30
+            "^\\d{1}/\\d{1}/\\d{2}$": {"C89": "%-d/%-m/%y", 'snowflake': 'dd/mm/yy', 'java': 'd/M/yy'},
+        }
+        datetime_dict = {
+            # 1930-08-05 12:00:05
+            "^\\d{4}-\\d{1,2}-\\d{1,2} \\d{2}:\\d{2}:\\d{2}$": {"C89": "%Y-%m-%d %H:%M:%S",
+                                                                'snowflake': 'yyyy-MM-dd hh:mi:ss',
+                                                                'java': 'yyyy-MM-dd HH:mm:ss'},
+            # 08-05-30 12:00:05
+            "^\\d{1,2}-\\d{1,2}-\\d{2}$ \\d{2}:\\d{2}:\\d{2}$": {"C89": "%d-%m-%y %H:%M:%S",
+                                                                 'snowflake': 'dd-mm-yy hh:mi:ss',
+                                                                 'java': 'dd-MM-yy HH:mm:ss'},
+            # 08-05-1930 12:00:05
+            "^\\d{1,2}-\\d{1,2}-\\d{4}$ \\d{2}:\\d{2}:\\d{2}$": {"C89": "%d-%m-%Y %H:%M:%S",
+                                                                 'snowflake': 'dd-mm-yyyy hh:mi:ss',
+                                                                 'java': 'dd-MM-yyyy HH:mm:ss'},
+            # 8-5-30 12:00:05
+            "^\\d{1}-\\d{1}-\\d{2}$ \\d{2}:\\d{2}:\\d{2}$": {"C89": "%-d/%-m/%y %H:%M:%S",
+                                                             'snowflake': 'dd-mm-yy hh:mi:ss',
+                                                             'java': 'd-M-yy HH:mm:ss'},
+            # 1930/08/05 12:00:05
+            "^\\d{4}/\\d{1,2}/\\d{1,2} \\d{2}:\\d{2}:\\d{2}$": {"C89": "%Y/%m/%d %H:%M:%S",
+                                                                'snowflake': 'yyyy/MM/dd hh:mi:ss',
+                                                                'java': 'yyyy/MM/dd HH:mm:ss'},
+            # 08/05/30 12:00:05
+            "^\\d{1,2}/\\d{1,2}/\\d{2} \\d{2}:\\d{2}:\\d{2}$": {"C89": "%d/%m/%y %H:%M:%S",
+                                                                'snowflake': 'dd/MM/yy hh:mi:ss',
+                                                                'java': 'dd/MM/yy HH:mm:ss'},
+            # 08/05/1930 12:00:05
+            "^\\d{1,2}/\\d{1,2}/\\d{4} \\d{2}:\\d{2}:\\d{2}$": {"C89": "%d-%m-%Y %H:%M:%S",
+                                                                'snowflake': 'dd/MM/yyyy hh:mi:ss',
+                                                                'java': 'dd/MM/yyyy HH:mm:ss'},
+            # 8/5/30 12:00:05
+            "^\\d{1}/\\d{1}/\\d{2} \\d{2}:\\d{2}:\\d{2}$": {"C89": "%-d/%-m/%y %H:%M:%S",
+                                                            'snowflake': 'dd/MM/yy hh:mi:ss',
+                                                            'java': 'd/M/yy HH:mm:ss'},
 
-    @pytest.mark.parametrize('return_formats, expected',
-                             [('snowflake', ['yyyy-mm-dd', 'yyyy/mm/dd']), ('java', ['yyyy-MM-dd', 'yyyy/MM/dd']),
-                              ('C89', ['%Y-%m-%d', '%Y/%m/%d'])])
-    def test_date_analyser(self, return_formats, expected):
-        """
-        Bad argument:
-        Special logic:
-        Boundary Values:
-        """
-        self.date_analyser_unittest(
-            expected_list=[{'formats': expected, 'type': 'date'}, {}, {}, {}, {}], data={
-                "date": ["2019-11-27",
-                         "2019/11/28",
-                         "2018-11-08"],
-                "gateway": ["PROSA", "PROSA", "PROSA"],
-                "amount": ["$4591", "$4592", "$4593"],
-                "order_id": [767313628196, 767313628196, 767313628196],
-                "nan": [np.nan, np.nan, np.nan]
-            }, return_format=return_formats)
+            # 2019-11-27 12:00:05.000
+            "^\\d{4}-\\d{1,2}-\\d{1,2} \\d{2}:\\d{2}:\\d{2}.\\d{3}$": {"C89": "%Y-%m-%d %H:%M:%S.%f",
+                                                                       'snowflake': 'yyyy-MM-dd HH:mm:ss.SS',
+                                                                       'java': "yyyy-MM-dd HH:mm:ss.SS"},
 
-    @pytest.mark.parametrize('return_formats, expected',
-                             [('snowflake', ['yyyy-MM-dd hh:mi:ss', 'yyyy/MM/dd hh:mi:ss', 'yyyy-MM-dd HH:mm:ss.SS']),
-                              ('java', ['yyyy-MM-dd HH:mm:ss', 'yyyy/MM/dd HH:mm:ss', 'yyyy-MM-dd HH:mm:ss.SS']),
-                              ('C89', ['%Y-%m-%d %H:%M:%S', '%Y/%m/%d %H:%M:%S', "%Y-%m-%d %H:%M:%S.%f"])])
-    def test_datetime_analyser(self, return_formats, expected):
-        """
-        Bad argument:
-        Special logic:
-        Boundary Values:
-        """
-        self.date_analyser_unittest(
-            expected_list=[{'formats': expected, 'type': 'datetime'}, {}, {},
-                           {}, {}], data={
-                "date": ["2019-11-27 12:00:00",
-                         "2019/11/28 12:00:00",
-                         '2020-12-01 14:11:34.000', np.nan],
-                "gateway": ["PROSA", "PROSA", "PROSA", "PROSA"],
-                "amount": ["$4591", "$4592", "$4593", "$4593"],
-                "order_id": [767313628196, 767313628196, 767313628196, 767313628196],
-                "nan": [np.nan, np.nan, np.nan, np.nan]
-            }, return_format=return_formats)
+            # 2019-11-27T12:00:05.000Z
+            "^\\d{4}-\\d{1,2}-\\d{1,2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}Z$": {"C89": "%Y-%m-%dT%H:%M:%S.%fZ",
+                                                                        'snowflake': 'yyyy-MM-ddTHH:mm:ss.SSZ',
+                                                                        'java': "yyyy-MM-dd'T'HH:mm:ss.SS'Z'"}
+
+        }
+        # Have this column a generic date format ?
+        if Evaluator(series).series_match(generic_date_pattern):
+            # Have this column time ?
+            format_type = 'datetime'
+            if Evaluator(series).series_contains(hour_pattern):
+                formats_dict = datetime_dict
+            else:
+                formats_dict = dates_dict
+                format_type = 'date'
+            format_result = {
+                'formats': [date_format.get(return_format) for (re_exp, date_format) in formats_dict.items() if
+                            Evaluator(series).series_contains(re_exp)], 'type': format_type}
+            return format_result
+        return {}
